dd1abeac9e53ca5235f3f8a47c56f74c6a5727d1
1n3ffbl3 <mizgier.m@gmail.com>
2019-03-06 14:14:33 +0100

    First functional component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = React.createElement(
-	'div',
-	{
-		className: 'wrapper'
-	},
-	[
-		React.createElement(
-			'h1',
-			{
-				className: 'mainHeader'
-			},
-			'Hello world'
-		),
-		React.createElement(
-			'h2',
-			{
-				className: 'secondaryHeader'
-			},
-			'Yellow'
-		)
-	]
+const App = () => (
+	/*In the case that your arrow function has a single expression 
+	as the function body, that expression will be executed, 
+	and the resulting value will be implicitly returned 
+	when the function is called */
+	// return (  no need of return
+	<div className="wrapper">
+		<h1 className="mainHeader"> Hello</ h1>
+		<h2 className="secondaryHeader"> yellow</h2>
+	</div >
+	// )
 );
 
 ReactDOM.render(
-	App,
+
+	< App />, // = App()
 
 	document.getElementById('root')
 );
