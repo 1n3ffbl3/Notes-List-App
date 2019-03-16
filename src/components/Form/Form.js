f4da8293ea296bd5bdbb4074154b66d2b0d6ab14
1n3ffbl3 <mizgier.m@gmail.com>
2019-03-16 20:44:40 +0100

    Creating dynamic Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,32 +3,92 @@ import styles from './Form.module.scss';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import Title from '../Title/Title';
+import Radio from './RadioForm';
 
-const Form = ({ submitFn }) => (
-	<div className={styles.wrapper}>
-		<Title>Add new twitter account</Title>
-		<form autoComplete="off" className={styles.form} onSubmit={submitFn}>
-			<Input
-				name="name"
-				label="Name"
-				maxLength={30}
-			/>
-			<Input
-				name="link"
-				label="Twitter Link"
-			/>
-			<Input
-				name="image"
-				label="Image"
-			/>
-			<Input
-				tag="textarea"
-				name="description"
-				label="Description"
-			/>
-			<Button>add new item</Button>
-		</form>
-	</div >
-);
+const types = {
+	twitter: 'twitter',
+	article: 'article',
+	note: 'note',
+}
+
+const descriptions = {
+	twitter: 'favorite Twitter account',
+	article: 'Article',
+	note: 'Note',
+}
+class Form extends React.Component {
+	state = {
+		type: types.twitter,
+		title: "",
+		link: "",
+		image: "",
+		description: "",
+	}
+
+	handleRadioButtonChange = (type) => {
+		this.setState({
+			type: type,
+		})
+	}
+
+	render() {
+		const { type } = this.state;
+
+		return (
+			<div className={styles.wrapper}>
+				<Title>Add new {descriptions[type]}</Title>
+				<form
+					autoComplete="off"
+					className={styles.form}
+					onSubmit={this.props.submitFn}>
+					<div className={styles.formOptions}>
+						<Radio
+							id={types.twitter}
+							checked={type === types.twitter}
+							changeFn={() => this.handleRadioButtonChange(types.twitter)}
+						>
+							Twitter
+					</Radio>
+						<Radio
+							id={types.article}
+							checked={type === types.article}
+							changeFn={() => this.handleRadioButtonChange(types.article)}
+						>
+							Article
+					</Radio>
+						<Radio
+							id={types.note}
+							checked={type === types.note}
+							changeFn={() => this.handleRadioButtonChange(types.note)}
+						>
+							Note
+					</Radio>
+					</div>
+					<Input
+						name="name"
+						label="Name"
+						maxLength={30}
+					/>
+					<Input
+						name="link"
+						label="Twitter Link"
+					/>
+					<Input
+						name="image"
+						label="Image"
+					/>
+					<Input
+						tag="textarea"
+						name="description"
+						label="Description"
+					/>
+					<Button>add new item</Button>
+				</form>
+			</div >
+		);
+	}
+}
+
+
 
 export default Form;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -18,7 +18,7 @@ const initialStateItems = [
 class Root extends React.Component {
 	state = {
 		items: [...initialStateItems],
-		isModalOpen: false,
+		isModalOpen: true,
 	};
 
 	addItem = event => {
